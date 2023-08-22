82a0c35f755056fd3e2538a1780ea86aea1e02c2
mtgto <hogerappa@gmail.com>
2023-08-22 23:36:43 +0900

    Initial Commit

---
+++ ExampleSevenZip/ExampleSevenZipApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSevenZipApp.swift
+//  ExampleSevenZip
+//
+//  Created by mtgto on 2023/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSevenZipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
